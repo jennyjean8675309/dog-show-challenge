373d7a4d0aeab297f16c2cd6135a894d6e3f9482
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-07-02 18:03:48 -0400

    completed Dog Show lab using OOJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+const DOGS_URL = 'http://localhost:3000/dogs'
 
+document.addEventListener('DOMContentLoaded', () => {
+    Dog.fetchDogs()
+    getDogForm().addEventListener('submit', Dog.updateDog)
 })
+
+function getTable() {
+    return document.querySelector('#table-body')
+}
+
+function getDogForm() {
+    return document.querySelector('#dog-form')
+}
